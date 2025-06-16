8c7c04a5a6959b98d3ec73f482e4b0de3f7e173e
stefanypaula <146445546+stefanypaula@users.noreply.github.com>
2025-06-16 08:33:07 -0300

    16/06

---
+++ main.js
@@ -0,0 +1,7 @@
+const numeroSenha = document. querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes =document.querySelectorAll('.parametro-senha_botao') ;
+
+console.log(botoes);
