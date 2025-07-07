c95a8f2035972ec7e652c649adc8a40f4cde2a7d
stefanypaula <146445546+stefanypaula@users.noreply.github.com>
2025-07-07 09:05:05 -0300

    aula 8

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ function geraSenha() {
         numeroAleatorio = Math.floor(numeroAleatorio);
         senha = senha + alfabeto[numeroAleatorio];
     }
-    campoSenha.value = senha; 
+    campoSenha.value = senha;
     classificaSenha();
 }
 
